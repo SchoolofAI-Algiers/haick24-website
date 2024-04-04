912e9e3d9bc3a43b92d4fd92a5e93627f6754cfd
lahcenbcf <h.bencharef@esi-sba.dz>
2024-04-04 16:42:43 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero/index.tsx";
 import Agenda from './agenda/agenda.jsx';
+import Mentors from "./components/mentors/Mentors.jsx";
 const App = () => {
 
 return(
     <div>
     <Hero />
       <Agenda/>
+      <Mentors />
     </div>
     
   )
