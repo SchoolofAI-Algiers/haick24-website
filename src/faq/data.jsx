0d5d99cafe63f0e26ac53d7165af5f42f92d371f
lahcenbcf <h.bencharef@esi-sba.dz>
2024-04-11 22:08:33 +0200

    add FAQ

--- src/faq/data.jsx
+++ src/faq/data.jsx
@@ -2,22 +2,32 @@
 
 export const faq = [
   {
-    question: "Question1",
+    question: "What is Haick24 about?",
     answer:
-      "Answer1",
+      "Haick24 is the third edition of Haick, a datathon designed for all AI and data science enthusiasts.",
   },
   {
-    question: "Question2",
-    answer:"Answer2"
+    question: "Who can participate?",
+    answer:"Anyone with AI skills and the ability to solve AI problems."
   },
   {
-    question: "Question3",
+    question: "How many members?",
     answer:
-      "Answer3",
+      "Each team should consist of 4 members.",
   },
   {
-    question: "Question4",
+    question: "How can I participate?",
     answer:
-      "Answer4",
+      "After forming your team, the team leader is asked to fill out the form on our website with all the information of their team. If accepted, we'll notify you in time.",
+  },
+  {
+    question: "Where and When?",
+    answer:
+      "Details about the location and date will be provided soon.",
+  },
+  {
+    question: "What are the prizes?",
+    answer:
+      "Information about prizes will be announced shortly.",
   },
 ];
