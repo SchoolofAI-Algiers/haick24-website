e0d6259cdd4601008dcd0c16067f42858d9c7455
Yasser <115426457+AssoulYasser@users.noreply.github.com>
2024-06-19 09:20:20 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/haick24-website",
+  base: "/haick24-website/",
   plugins: [react()]
 })
