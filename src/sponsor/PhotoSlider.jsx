021112a793262a813f84469618a31075bf47902d
lahcenbcf <h.bencharef@esi-sba.dz>
2024-06-25 21:40:13 +0100

    add sponsors

--- src/sponsor/PhotoSlider.jsx
+++ src/sponsor/PhotoSlider.jsx
@@ -1,44 +1,22 @@
 // PhotoSlider.jsx
-import React from 'react';
-import { motion } from 'framer-motion';
-import left from './logo/left1.svg';
-import right from './logo/right1.svg';
-import sonatrach from './logo/sonatrach.svg';
-import nvidia from './logo/nvidia.svg';
-import ai from './logo/ai.svg';
-
-const photos = [
+import ats from "./logo/ats.svg"
+/*const photos = [
   sonatrach,
   nvidia,
   ai,
   // Add more photo URLs here
 ];
-
+*/
 const Sponsors = () => {
-  const duplicatedPhotos = [
-    ...photos,
-    ...photos,
-    ...photos,
-    ...photos,
-    ...photos,
-    ...photos,
-  ];
+  
 
   return (
     <div id='sponsors' className="w-full bg-skyBlueColor pb-20 text-white relative ">
-      <img src={right} alt="" className="absolute  -top-32 left-0   z-[10]" />
-      <img src={left} alt="" className="absolute  -top-32 right-0   z-[10]" />
-
+      
       <h1 className=" text-6xl  font-bold text-center py-20">Our Sponsors</h1>
-       <p className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[50%] font-bold text-white text-[2rem] md:text-[2.5rem] lg:text-[3rem]">COMING SOON</p>
-      <div className="flex justify-center blur-md opacity-50">
-        <div
-          className="md:w-3/4 mx-auto sm:w-4/5 lg:w-3/4 w-4/5 inline-flex flex-nowrap overflow-hidden "
-          style={{
-            maskImage:
-              'linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)',
-          }}
-        >
+      <div className="flex justify-center">
+    
+          {/*
           <motion.div
             className="flex"
             animate={{
@@ -63,7 +41,10 @@ const Sponsors = () => {
               </div>
             ))}
           </motion.div>
-        </div>
+          
+          */}
+          
+          <img src={ats} className="w-full h-auto object-cover -mt-32" />
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/haick24-website/",
+  assetsInclude:["**/*.ai"],
   plugins: [react()]
 })
